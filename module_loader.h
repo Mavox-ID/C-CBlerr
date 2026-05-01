5eca0b55a991ba8f3ef042bcdb9faf58f5ab6804
Dont Forget All Please <markd.voznyuk@gmail.com>
2026-05-01 14:33:02 +0300

    Delete module_loader.h

--- module_loader.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "ast.h"
-
-/* Resolves and inlines all import statements into prog.
-   source_path is the path of the file being compiled. */
-int module_inline_imports(Program *prog, const char *source_path);
